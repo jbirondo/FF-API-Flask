1599048e9b781754ec0cfce1bbc7819b503b394b
Eugene Birondo <jbirondo@gmail.com>
2020-09-02 23:22:29 -0700

    Debugging deploy

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import flask
 from flask import request, jsonify, json
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 
 pprplayers = open("ppr.json", "r")
 halfpprplayers = open("halfppr.json", "r")
