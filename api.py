87d0ebadc3840e2e68df49d93fefe90f6127b03f
Eugene Birondo <jbirondo@gmail.com>
2020-09-02 18:59:43 -0700

    Adding standard and halfppr

--- api.py
+++ api.py
@@ -5,7 +5,12 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 pprplayers = open("ppr.json", "r")
+halfpprplayers = open("halfppr.json", "r")
+standardplayers = open("standard.json", "r")
+
 pprdata = json.loads(pprplayers.read())
+halfpprdata = json.loads(halfpprplayers.read())
+standarddata = json.loads(standardplayers.read())
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,5 +20,13 @@ def home():
 @app.route('/api/ppr', methods=['GET'])
 def api_ppr():
     return json.dumps(pprdata)
+
+@app.route('/api/standard', methods=['GET'])
+def api_standard():
+    return json.dumps(standarddata)
+
+@app.route('/api/halfppr', methods=['GET'])
+def api_halfppr():
+    return json.dumps(halfpprdata)
 
 app.run()
