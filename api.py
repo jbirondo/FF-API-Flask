278b0c822affb6eab1120cf8085eda559b556f93
Eugene Birondo <jbirondo@gmail.com>
2020-09-02 18:35:50 -0700

    Changing variables

--- api.py
+++ api.py
@@ -4,12 +4,12 @@ from flask import request, jsonify, json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-players = open("index.json", "r")
+pprplayers = open("ppr.json", "r")
 pprdata = json.loads(players.read())
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>VOR Fantasy Football API</h1>"
 
 
 @app.route('/api/all', methods=['GET'])
